5b7ffb98eb3c5eb7fa51ff85df23818cc3281625
Imatdinov <Imatdinov@tecon.ru>
2021-01-27 20:08:23 +0300

    добавлен просморт загружаемых файлов, а так же исправлена ошибка работы с файлами настроек

--- main.py
+++ main.py
@@ -16,11 +16,15 @@ from mainwind import Ui_Dialog
 import threading
 import configparser
 import psutil
+from qbittorrent import Client
+
+qb = Client('http://127.0.0.1:8080/')
 
 threadStop = False
 log = None
 
 bot = telebot.TeleBot(bottoken.TOKEN)  # You can set parse_mode by default. HTML or MARKDOWN
+downloadTorrents = []
 
 
 class settingsBotCl:
@@ -38,11 +42,32 @@ settingsBot = settingsBotCl(config.DOWNLOADPATH)
 
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, '/qtinfo \n /qtDownloadInfo')
+
+
+@bot.message_handler(commands=['qtinfo'])
+def tor_info_message(message):
+    qb.login('admin', '1karina1')
+    torrents = qb.torrents()
+    for torrent in torrents:
+        bot.send_message(message.chat.id, torrent['name'])
+    qb.logout()
+
+
+@bot.message_handler(commands=['qtDownloadInfo'])
+def tor_info_message(message):
+    qb.login('admin', '1karina1')
+    torrents = qb.torrents(filter='downloading')
+    if not torrents:
+        bot.send_message(message.chat.id, 'Нет не загруженных торрентов')
+        return
+    for torrent in torrents:
+        bot.send_message(message.chat.id, torrent['name'])
+    qb.logout()
 
 
 @bot.message_handler(content_types='document')
-def start_message(message):
+def torrent_message(message):
     if message.document.file_name.find(".torrent") < 0:
         mes = "Прислан не тот документ"
         bot.send_message(message.chat.id, mes)
@@ -56,11 +81,13 @@ def start_message(message):
     if not filepath:
         os.mkdir(config.DOWNLOADPATH)
     downloaded_file = bot.download_file(newFile.file_path)
-    src = message.document.file_name
-    with open(config.DOWNLOADPATH + "/" + src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    mes = "Файл сохранен в " + config.DOWNLOADPATH
-    bot.send_message(message.chat.id, mes)
+
+
+    # src = message.document.file_name
+    # with open(config.DOWNLOADPATH + "/" + src, 'wb') as new_file:
+    #     new_file.write(downloaded_file)
+    # mes = "Файл сохранен в " + config.DOWNLOADPATH
+    # bot.send_message(message.chat.id, mes)
 
     # проверим запущен ли bittorrent
     find_process = False
@@ -72,6 +99,31 @@ def start_message(message):
     # если торрент клиент не запщен то запускаем
     if not find_process:
         os.startfile(config.TORRENTCLIENTPATH)
+        time.sleep(3)
+    qb.login('admin', '1karina1')
+    downTorrents = qb.torrents(filter='downloading')
+    qb.download_from_file(downloaded_file)
+    time.sleep(2)
+    downAddedTorrents = qb.torrents(filter='downloading')
+    findtorr = {}
+    cont = False
+    # поиск добавленного файла
+    for addtorr in downAddedTorrents:
+        for notaddtorr in downTorrents:
+            if addtorr['name'] == notaddtorr['name']:
+                #downAddedTorrents.remove(addtorr)
+                #downTorrents.remove(notaddtorr)
+                cont = True
+                break
+        if cont:
+            cont = False
+            continue
+        findtorr = addtorr
+        break
+
+    if findtorr:
+        downloadTorrents.append({'userid': message.chat.id, 'name': findtorr['name']})
+    qb.logout()
 
 
 @bot.message_handler(func=lambda message: True)
@@ -102,6 +154,37 @@ def thread_function(name):
             print(e)
             log.append("Ошбика")
             time.sleep(3)
+
+
+def thread_torr(name):
+    log.append("Поток запущен, бот работает")
+    while True:
+        if threadStop:
+            # bot.stop_polling()
+            log.append("Бот остановлен: поток")
+            break
+        try:
+            cont = False
+            qb.login('admin', '1karina1')
+            torrents = qb.torrents(filter='downloading')
+            for dtor in downloadTorrents:
+                for tor in torrents:
+                    if tor['name'] == dtor['name']:
+                        cont = True
+                        break
+                if cont:
+                    cont = False
+                    continue
+                bot.send_message(dtor['userid'], "Файл " + dtor['name'] + "загружен")
+            
+            qb.logout()
+            time.sleep(20)
+
+        except Exception as e:
+            print(e)
+            log.append("Ошбика")
+            time.sleep(3)
+
 
 class MainWindow(QMainWindow):
     check_box = None
@@ -221,7 +304,11 @@ class MainWindow(QMainWindow):
         if not os.path.isfile(setdir):
             return
         configp = configparser.ConfigParser()
-        configp.read(setdir)
+        try:
+            configp.read(setdir)
+        except Exception as e:
+            os.remove(setdir)
+
         if configp.get("DEFAULT", "minimizeToTray") == "yes":
             self.ui.trayCeckBox.setChecked(True)
         if configp.get("DEFAULT", "autostartBot") == "yes":
